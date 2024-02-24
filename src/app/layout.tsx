d8ce27d4bd6c1303a7b2aa45b846d7f68a50310f
M0nadic.Ph0 <m0nadic.ph0@gmail.com>
2024-02-24 23:01:27 +0530

    auth context and provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { AuthContextProvider } from "@/context/AuthContext";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -20,7 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-        {children}
+        <AuthContextProvider>
+          {children}
+        </AuthContextProvider>
       </body>
     </html>
   );
---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,28 @@
+import { ReactNode, createContext, useReducer } from "react";
+
+
+export const AuthContext = createContext(null)
+
+export const authReducer = (state: any, action: any) => {
+    switch (action.type) {
+        case 'LOGIN':
+            return {user: action.payload}
+        case 'LOGOUT':
+            return {user: null}
+        default:
+            return state
+    }
+}
+
+export const AuthContextProvider = ({children}:{children: ReactNode}) => {
+    const [state, dispatch] = useReducer(authReducer, {
+        user: null
+    })
+    console.log('AuthContext state:', state);
+    
+    return (
+        <AuthContext.Provider value={{...state}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
