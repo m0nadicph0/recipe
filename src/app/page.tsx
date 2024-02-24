615e09989edbfabc78c9a842b4501628a2ea2d74
M0nadic.Ph0 <m0nadic.ph0@gmail.com>
2024-02-24 17:13:19 +0530

    recipe cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import Image from "next/image";
 
 
 interface Recipe {
@@ -26,6 +28,12 @@ export default async function Home() {
         {recipes.map(recipe => (
          <Card key={recipe.id} className="flex flex-col justify-between">
             <CardHeader className="flex-row gap-4 items-center">
+              <Avatar>
+                <AvatarImage src={`/img/${recipe.image}`}/>
+                <AvatarFallback>
+                  {recipe.title.slice(0,2)}
+                </AvatarFallback>
+              </Avatar>
               <div>
                 <CardTitle>{recipe.title}</CardTitle>
                 <CardDescription>{recipe.time} mins to cook.</CardDescription>
@@ -35,8 +43,8 @@ export default async function Home() {
               {recipe.description}
             </CardContent>
             <CardFooter className="flex justify-between">
-              <button>View Recipe</button>
-              {recipe.vegan && <p>Vegan!</p>}
+              <Button>View Recipe</Button>
+              {recipe.vegan && <Badge variant="secondary">VEGAN</Badge>}
             </CardFooter>
          </Card>
         ))}
