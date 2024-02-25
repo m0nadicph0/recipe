2a869c542f829622dd066120ed867463f6908985
M0nadic.Ph0 <m0nadic.ph0@gmail.com>
2024-02-25 09:59:58 +0530

    useLogout hook

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,10 +1,20 @@
-import React, { ReactNode } from 'react'
+'use client'
+import { Button } from '@/components/ui/button'
+import { useLogout } from '@/hooks/useLogout'
+import React, { MouseEvent, ReactNode } from 'react'
 
 export default function ProtectedLayout({children}: {children: ReactNode}) {
+  const {logout} = useLogout()
+
+  function handleLogout(event: MouseEvent<HTMLButtonElement>) {
+    logout()
+  }
+
   return (
     <div>
        <nav>
           <h1>Recipes for Ninjas</h1>
+          <Button onClick={handleLogout}>Log Out</Button>
         </nav>
       {children}
     </div>
